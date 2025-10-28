d58d75700a48297d74ea1a404ab427152caea59d
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2025-10-28 21:37:15 +0900

    streetViewPointMarkerのスタイルを調整

--- src/components/molecule/googlemap/StreetViewPointMarker.tsx
+++ src/components/molecule/googlemap/StreetViewPointMarker.tsx
@@ -10,16 +10,15 @@ export function StreetViewPointMarker({map, latLng}: StreetViewPointMarkerProps)
   const markerRef = useRef(null)
 
   const content = document.createElement('div');
-  content.innerHTML = 'SV';
+  content.innerHTML = '';
   content.style.backgroundColor = "#3B82F6";
+  content.style.border = '2px solid white';
+  content.style.boxShadow = '0 0 5px #3B82F6';
   content.style.borderRadius = '50%';
-  content.style.color = 'white';
-  content.style.fontSize = '16px';
-  content.style.fontWeight = 'bold';
   content.style.textAlign = 'center';
-  content.style.lineHeight = '10px';
-  content.style.width = '10px';
-  
+  content.style.height = '20px';
+  content.style.width = '20px';
+
   return (
     <AdvancedMarker map={map} markerRef={markerRef} content={content} options={{
       position: latLng,
