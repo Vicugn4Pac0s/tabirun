576f23155077dbe94ed54cd0cc70d49d9486f82b
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2025-10-28 21:52:23 +0900

    streetView位置変更をクリックで（暫定的に）

--- src/components/organism/MapRoot.tsx
+++ src/components/organism/MapRoot.tsx
@@ -6,6 +6,7 @@ import StreetViewPointMarker from "../molecule/googlemap/StreetViewPointMarker";
 import { useStreetViewPanoramaStore } from "~/app/stores/googlemap/streetViewPanoramaStore";
 
 function MapRoot() {
+  const streetViewPanorama = useStreetViewPanoramaStore((state) => state.streetViewPanorama);
   const streetViewPanoramaCenter = useStreetViewPanoramaStore((state) => state.streetViewPanoramaCenter);
   const map = useMapStore((state) => state.map);
   const setDirectionsService = useMapStore((state) => state.setDirectionsService);
@@ -23,9 +24,9 @@ function MapRoot() {
 
   const onClick = (e: google.maps.MapMouseEvent) => {
     const latLng = e.latLng;
-    if(!latLng) return; 
+    if(!latLng || !streetViewPanorama) return; 
     const latLngLiteral = latLng.toJSON() as google.maps.LatLngLiteral;
-
+    streetViewPanorama.setPosition(latLngLiteral);
     // if(page !== 'register') return;
     // addRoutePoint(latLngLiteral);
   };
