b8b82a32668b0d438c05215c0596b60104586861
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2026-02-11 21:35:44 +0900

    認証機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { HydrateClient } from "~/trpc/server";
+import { SessionProvider } from 'next-auth/react';
 import Root from "~/frontend/components/organism/Root";
 
 export default async function Home() {
 
   return (
     <HydrateClient>
-      <main>
-        <Root />
-      </main>
+      <SessionProvider>
+        <main>
+          <Root />
+        </main>
+      </SessionProvider>
     </HydrateClient>
   );
 }
--- src/frontend/components/organism/Root.tsx
+++ src/frontend/components/organism/Root.tsx
@@ -1,18 +1,20 @@
 'use client';
 
+import { signIn, signOut, useSession } from "next-auth/react";
 import MapRoot from "./MapRoot";
 import RouteNavigator from "./RouteNavigator";
 import StreetViewPanoramaWrapper from "./StreetViewPanoramaWrapper";
 import Sidebar from "./Sidebar";
 
 function Root() {
+  const {data: session} = useSession();
 
   return (
     <div className="flex">
       <div className="w-72">
         <Sidebar />
       </div>
-      <div className="flex-1 grid grid-cols-2">
+      <div className="flex-1 grid grid-cols-2 relative">
         <div className="relative">
           <StreetViewPanoramaWrapper />
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-10">
@@ -21,6 +23,17 @@ function Root() {
         </div>
         <div>
           <MapRoot />
+        </div>
+        <div className="absolute top-3 right-3 z-50">
+          {session?.user ? (
+            <button className="rounded-full overflow-hidden h-11 w-11" onClick={() => signOut()}>
+              <img src={session.user.image || ''} alt="" />
+            </button>
+          ) : (
+            <button className="text-gray-700 text-5xl align-middle" onClick={() => signIn()}>
+              ログイン
+            </button>
+          )}
         </div>
       </div>
     </div>
